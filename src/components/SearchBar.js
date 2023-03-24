0c6a6a0757060d61f13cbed53c5bc6254c96bce8
willdoesprojects <sunsoffire@gmail.com>
2023-03-24 17:47:10 -0400

    modified:   src/components/SearchBar.js 	new file:   src/components/SignIn.css 	modified:   src/components/SignIn.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -23,13 +23,13 @@ function SearchBar({placeholder, data}) {
     return (
         <div className ="search relative h-screen">
             <div className="searchInputs absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5">
-                <input type="text" class = "w-full border-12 border-black rounded-full py-2 px-4 outline-transparent text-black dark:text-black-dark" placeHolder={placeholder} onChange={handleFilter} maxLength={15}/>
+                <input type="text" class = "w-full border-12 border-black rounded-full py-2 px-4 outline-black text-black dark:text-black-dark" placeHolder={placeholder} onChange={handleFilter} maxLength={15}/>
                 <div className="searchIcon absolute top-1/2 transform -translate-y-1/2 right-4">
                     <SearchIcon />
                 </div>
             </div>
             {filiteredData.length != 0 && (
-            <div className = " ">
+            <div className = "dataResult absolute bottom-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5">
                 {filiteredData.slice(0, 5).map((value, key) => {
                     return <div>{value.groccery} </div>;
                 })}
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-
+import logo from './logo.PNG';
+import './SignIn.css'
 function SignIn() {
     return (
+        
+        
         <Form>
+            <div className = "imageLogo">
+            <img src={logo} />
+            </div>
             <Form.Group className="mb-8 w-1/3 focus:border-[#004e36]">
                 <Form.Control placeHolder="Email"></Form.Control>
             </Form.Group>
-            <Form.Group className = "w-1/3">
+            <Form.Group className="w-1/3">
                 <Form.Control placeHolder="Password"></Form.Control>
             </Form.Group>
         </Form>
