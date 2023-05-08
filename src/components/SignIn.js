ede66334a040db5898f1e345d8b81b5d81af8378
willdoesprojects <sunsoffire@gmail.com>
2023-05-08 18:09:36 -0400

    modified:   src/components/SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,18 +5,28 @@ import logo from './logo.PNG';
 import './SignIn.css'
 function SignIn() {
     return (
-        
-        
         <Form>
             <div className = "imageLogo">
             <img src={logo} />
             </div>
-            <Form.Group className="mb-8 w-1/3 focus:border-[#004e36]">
-                <Form.Control placeHolder="Email"></Form.Control>
-            </Form.Group>
-            <Form.Group className="w-1/3">
-                <Form.Control placeHolder="Password"></Form.Control>
-            </Form.Group>
+
+            <div class="form-group">
+                <label for="emailInput">Email address</label>
+                <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" />
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
         </Form>
     );
 }
